437d66884360cf4eb5188fb62ca9a38e5c231aeb
Luis López <dg.luislo8@gmail.com>
2025-11-10 21:57:22 -0800

    Agregue la clase Categoria

--- src/proyectofinal/model/Autor.java
+++ src/proyectofinal/model/Autor.java
@@ -9,5 +9,22 @@ package proyectofinal.model;
  * @author venga
  */
 public class Autor {
+    private int id;
+    private String nombre;
+    private String nacionalidad;
     
+    public Autor(){}
+    
+    public Autor (int id, String nombre, String nacionalidad ){
+        this.id = id;
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+    }
+    
+    public int getId(){return id;}
+    public void setId(int id){this.id = id;}
+    public String getNombre(){return nombre;}
+    public void setNombre(String nombre){this.nombre = nombre;}
+    public String getNacionalidad() {return nacionalidad;}
+    public void setNacionalidad(String nacionalidad){this.nacionalidad = nacionalidad;}
 }
